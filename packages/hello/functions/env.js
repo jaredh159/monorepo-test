c35d4bf8d671ed11d4ba6690a3d15d1798488739
Jared Henderson <jared@netrivet.com>
2020-04-06 17:23:22 -0400

    log more things

--- packages/hello/functions/env.js
+++ packages/hello/functions/env.js
@@ -1,7 +1,17 @@
 exports.handler = function (event, context, callback) {
   console.log(process.env);
+  console.log(event);
+  console.log(context);
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify(process.env, null, 2),
+    body: JSON.stringify(
+      {
+        env: process.env,
+        event,
+        context,
+      },
+      null,
+      2
+    ),
   });
 };
